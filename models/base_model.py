3ef056049b5c60444859a5a6d1e7ca074eec978f
abdel2000-dply <abno870@gmail.com>
2023-11-09 14:23:30 +0100

    task 4: Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,19 @@ class BaseModel:
     defines all common attributes/methods for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Initialize a new instance of BaseModel """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for k, v in kwargs.items():
+                if k == 'created_at' or k == 'updated_at':
+                    vformated = datetime.strptime(v, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, k, vformated)
+                elif k != '__class__':
+                    setattr(self, k, v)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ string representation of the BaseModel """
