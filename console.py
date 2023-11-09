d3e3a12d14aa444797ed2e9e3afbb1d9aa10304c
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2023-11-08 20:29:08 -0800

    create the command interpreter class cmd module

--- console.py
+++ console.py
@@ -4,20 +4,20 @@ import cmd
 
 class HBNBCommands(cmd.Cmd):
     """This defines the class that inherits from the cmd module"""
-    intro = "Alx first introducton to AirBnB_clone project"
+    intro = "Welcome to AirBnB_clone project"
 
     prompt = "(hbnb) "
 
     def default(self, line):
         """invoked when the users Enters nothing"""
-        return True
+        pass
 
     def do_quit(self, line):
         """exit the shell"""
         return True
     def do_EOF(self, line):
         """handles when an EOF is encountered"""
-        return true
+        return True
     def do_help(self, line):
         """invokes the command for help"""
         print()
