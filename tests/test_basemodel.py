fa7b0940a1bc34b336e9edaefb1fa3066db02beb
abdel2000-dply <abno870@gmail.com>
2023-11-12 14:55:33 +0100

    testing

---
+++ tests/test_basemodel.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Unittests for base_model """
+"""
+import os
+import models
+import unittest
+from datetime import datetime
+from time import sleep
+from models.base_model import BaseModel
+
+"""
